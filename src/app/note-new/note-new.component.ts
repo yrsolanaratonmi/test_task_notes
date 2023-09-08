aced8473ddfdb54ed164f6e1433f5beed24d751b
barannikrs <barannikrs@pik.ru>
2023-09-08 23:40:42 +0400

    fix: fix []

--- src/app/note-new/note-new.component.ts
+++ src/app/note-new/note-new.component.ts
@@ -1,9 +1,9 @@
-import { Component, ContentChild, ElementRef, Input } from '@angular/core';
+import { Component, ContentChild, ElementRef, EventEmitter, Input, Output } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {map} from 'rxjs/internal/operators/map';
 import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 import {Observable, filter, switchMap} from 'rxjs';
-import {Store} from '@ngxs/store';
+import {Select, Store} from '@ngxs/store';
 import {AddNote, EditNote, Note, NotesState} from '../store/notes.state';
 
 @Component({
@@ -15,16 +15,24 @@ export class NoteNewComponent {
 
   @ContentChild('save') public save! : ElementRef;
 
+  @Select(NotesState) notes$!: Observable<Array<Note>>
+
+  @Output() close = new EventEmitter()
+
   public noteData = new FormGroup({
     title: new FormControl('', Validators.required),
-    description: new FormControl('', Validators.required)
+    description: new FormControl('', Validators.required),
+    file: new FormControl(null)
   })
+
+  file: any;
 
   id = this.route.snapshot.params['id'];
 
+  isEditing = this.route.snapshot.routeConfig?.path?.includes('edit')
+
   constructor (private readonly store: Store, private readonly router: Router, private readonly route: ActivatedRoute) {}
 
-  private notes$ = this.store.select(NotesState)
 
   ngOnInit(): void {
     this.getSingle()
@@ -32,22 +40,26 @@ export class NoteNewComponent {
       map((route: any) => +route.url?.slice(1) || +route.routerEvent?.url?.slice(1)),
       switchMap(routeId => {
         this.id = routeId
-        return this.store.select(NotesState).pipe(
+        return this.notes$.pipe(
           map((notes: Array<Note>) => notes.find((note: Note) => note.id === routeId))
         )
       }),
     ).subscribe(res => {
       this.noteData.controls.title.setValue(res?.title as string)
       this.noteData.controls.description.setValue(res?.description as string)
+      this.noteData.controls.file.setValue(res?.file)
+      this.file = res?.file
     })
   }
 
   getSingle () {
-    this.store.select(NotesState).pipe(
+    this.notes$.pipe(
       map((notes: Array<Note>) => notes.find((note: Note) => note.id === +this.id))
     ).subscribe(res => {
       this.noteData.controls.title.setValue(res?.title as string)
       this.noteData.controls.description.setValue(res?.description as string)
+      this.noteData.controls.file.setValue(res?.file)
+      this.file = res?.file
     })
   }
 
@@ -61,17 +73,35 @@ export class NoteNewComponent {
     const { title, description } = this.noteData.value
     data.title = title
     data.description = description
+    data.file = this.file;
     data.created = new Date()
     this.store.dispatch(new AddNote(data))
     this.router.navigate([data.id])
+    this.close.emit()
   }
 
   saveEdit() {
     const {title, description} = this.noteData.value
     const id = +this.id
     const created = new Date()
-    const data = { id, title, description, created }
+    const file = this.file
+    const data = { id, title, description, created, file }
     this.store.dispatch(new EditNote(data as Note))
-    this.router.navigate([''])
+    this.router.navigate([id])
+    this.close.emit()
+  }
+
+  onFileChange() {
+    const reader = new FileReader();
+
+    reader.onload = (event: ProgressEvent) => {
+      this.file = (event.target as any).result
+    };
+
+    reader.readAsDataURL(this.noteData.controls.file.value as any);
+  }
+
+  removeFile() {
+    this.noteData.controls.file.setValue(null)
   }
 }
